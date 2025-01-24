8c3920c0861450e64fba9e6be3912200f89f07b2
VivekSolanki2000 <viveksn21@gmail.com>
2025-01-24 09:08:40 -0500

    function headers and comments added.

--- Main.cpp
+++ Main.cpp
@@ -2,28 +2,40 @@
 #include "NetworkSettings.hh"
 #include "RemoteManagement.hh"
 
-int main(int argc, char* argv[]) {
-    
-    if (argc < 2 || (argc < 3 && (strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "-b") == 0))) {
-        cerr << "Usage:\n" 
-                  << argv[0] << " -s            (for server)\n"
-                  << argv[0] << " -c server_ip  (for client)" << endl;
+int main(int argc, char *argv[])
+{
+    // To start application must receive the parameter to define behaviour of application as it can act as either server or client.
+    /*
+     *  To run application as server : ./tcpapp -s
+     *  To run application as client : ./tcpapp -c ipaddress_of_server
+     */
+    if (argc < 2 || (argc < 3 && (strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "-b") == 0)))
+    {
+        cerr << "Usage:\n"
+             << argv[0] << " -s            (for server)\n"
+             << argv[0] << " -c server_ip  (for client)" << endl;
         return 1;
     }
-
 
     NetworkSettings network;
     string mode = argv[1];
 
-    if (mode == "-s") {
-        if (!network.initializeAsServer()) return 1;
+    if (mode == "-s")
+    {
+        if (!network.initializeAsServer())
+            return 1;
+
         network.runServer();
-    } 
-    else if (mode == "-c" && argc == 4) {
-        if (!network.initializeAsClient(argv[2])) return 1;
+    }
+    else if (mode == "-c" && argc == 4)
+    {
+        if (!network.initializeAsClient(argv[2]))
+            return 1;
+
         network.runClient();
     }
-    else {
+    else
+    {
         cerr << "Invalid arguments" << endl;
         return 1;
     }
--- NetworkSettings.cpp
+++ NetworkSettings.cpp
@@ -6,31 +6,59 @@
 #include <thread>
 #include "NetworkSettings.hh"
 
-NetworkSettings::NetworkSettings() : sock(0) {}
+/* In Constructor initialise variables of class */
+NetworkSettings::NetworkSettings() : sock(0)
+{
+}
 
-void NetworkSettings::handleClient(int clientSocket) {
+/*********************************************************************
+ * @fn      		  - handleClient
+ * @brief             - This function handles the receiving data from the client and
+ *                      sends a response
+ * @param[in]         - int clientSocket
+ * @return            - None
+ * @Note              -
+ *********************************************************************/
+void NetworkSettings::handleClient(int clientSocket)
+{
     char buffer[BUFFER_SIZE] = {0};
-    while (true) {
+    while (true)
+    {
         memset(buffer, 0, BUFFER_SIZE);
         int valRead = read(clientSocket, buffer, BUFFER_SIZE);
-        
-        if (valRead <= 0) break;
-        
+
+        if (valRead <= 0)
+            break;
+
         cout << "Message from client " << clientSocket << ": " << buffer << endl;
-        const char* response = "Message received";
+
+        // Send client a response
+        const char *response = "Message received";
         send(clientSocket, response, strlen(response), 0);
     }
     close(clientSocket);
 }
 
-bool NetworkSettings::initializeAsServer() {
+/*********************************************************************
+ * @fn      		  - initializeAsServer
+ * @brief             - This function initialise the application in
+ *                      server mode with configured parameter and waits
+ *                      for client to connect on 8080 port
+ * @param[in]         - none
+ * @return            - bool
+ * @Note              -
+ *********************************************************************/
+bool NetworkSettings::initializeAsServer()
+{
     int opt = 1;
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         cerr << "Socket creation failed" << endl;
         return false;
     }
 
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
         cerr << "Setsockopt failed" << endl;
         return false;
     }
@@ -39,12 +67,14 @@ bool NetworkSettings::initializeAsServer() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    if (bind(sock, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(sock, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         cerr << "Bind failed" << endl;
         return false;
     }
 
-    if (listen(sock, 10) < 0) {
+    if (listen(sock, 10) < 0)
+    {
         cerr << "Listen failed" << endl;
         return false;
     }
@@ -53,23 +83,47 @@ bool NetworkSettings::initializeAsServer() {
     return true;
 }
 
-void NetworkSettings::runServer() {
+/*********************************************************************
+ * @fn      		  - initializeAsServer
+ * @brief             - This function accepts all the client conne ction
+ *                      and runs a new thread for each of them and stores
+ *                      thread details in clientThreads vector
+ * @param[in]         - none
+ * @return            - none
+ * @Note              -
+ *********************************************************************/
+void NetworkSettings::runServer()
+{
     int addrlen = sizeof(address);
-    
-    while (true) {
-        int clientSocket = accept(sock, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-        if (clientSocket < 0) {
+
+    while (true)
+    {
+        int clientSocket = accept(sock, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if (clientSocket < 0)
+        {
             cerr << "Accept failed" << endl;
             continue;
         }
-        
+
         cout << "New client connected: " << clientSocket << endl;
+        // NOTE: emplace_back constructs the new element in place using the arguments provided. This avoids the extra copy or move operation required when using push_back.
         clientThreads.emplace_back(&NetworkSettings::handleClient, this, clientSocket);
     }
 }
 
-bool NetworkSettings::initializeAsClient(const char* serverIP) {
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+/*********************************************************************
+ * @fn      		  - initializeAsClient
+ * @brief             - This function initialise the application in
+ *                      client mode with configured parameter and
+ *                      tries to connect with server based on given serverIP
+ * @param[in]         - const char *serverIP
+ * @return            - bool
+ * @Note              -
+ *********************************************************************/
+bool NetworkSettings::initializeAsClient(const char *serverIP)
+{
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         cerr << "Socket creation failed" << endl;
         return false;
     }
@@ -77,12 +131,14 @@ bool NetworkSettings::initializeAsClient(const char* serverIP) {
     address.sin_family = AF_INET;
     address.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, serverIP, &address.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, serverIP, &address.sin_addr) <= 0)
+    {
         cerr << "Invalid address" << endl;
         return false;
     }
 
-    if (connect(sock, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (connect(sock, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         cerr << "Connection failed" << endl;
         return false;
     }
@@ -91,14 +147,25 @@ bool NetworkSettings::initializeAsClient(const char* serverIP) {
     return true;
 }
 
-void NetworkSettings::runClient() {
+/*********************************************************************
+ * @fn      		  - runClient
+ * @brief             - This function handles the communication part on
+ *                      client side
+ * @param[in]         - none
+ * @return            - none
+ * @Note              -
+ *********************************************************************/
+void NetworkSettings::runClient()
+{
     char buffer[BUFFER_SIZE] = {0};
-    while (true) {
+    while (true)
+    {
         string message;
         getline(cin, message);
-        
-        if (message == "quit") break;
-        
+
+        if (message == "quit")
+            break;
+
         send(sock, message.c_str(), message.length(), 0);
         memset(buffer, 0, BUFFER_SIZE);
         read(sock, buffer, BUFFER_SIZE);
@@ -106,9 +173,13 @@ void NetworkSettings::runClient() {
     }
 }
 
-NetworkSettings::~NetworkSettings() {
-    for (auto& thread : clientThreads) {
-        if (thread.joinable()) thread.join();
+/* In Destructor De-initialise variables of class */
+NetworkSettings::~NetworkSettings()
+{
+    for (auto &thread : clientThreads)
+    {
+        if (thread.joinable())
+            thread.join();
     }
     close(sock);
 }
